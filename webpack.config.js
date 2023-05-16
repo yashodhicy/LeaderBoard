bd4a70a6d92d34c5b3e3addfdaa1bac00f51fecb
yashodhi Chathurangi <yashodhichathurangi94@gmail.com>
2023-05-16 22:52:12 +0300

    fix linters

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -11,6 +11,14 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/images'),
+          to: 'images',
+        },
+      ],
     }),
   ],
   output: {
